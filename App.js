d9dc96271b965d0f548bfc30a75ac82b6e00a838
furkan ataman <vandeles_furkan_1996@hotmail.com>
2022-06-18 22:10:46 +0300

    todo start

---
+++ App.js
@@ -0,0 +1,91 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import { Component, Node, } from 'react';
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import ItemList from './ItemList';
+
+export default class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      data: []
+    }
+  }
+  handleSave = () => {
+    const { text, data } = this.state;
+    data.push({ text });
+    this.setState({ data, text: '' });
+    console.log(this.state.data);
+  };
+
+  render() {
+    const { text, data } = this.state
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={styles.title}>
+          <Text style={styles.title_text}>To-Do Application</Text></View>
+          <View
+            style={{ backgroundColor: '#ccc', padding: 10, flexDirection: 'row' }}>
+            <TextInput style={styles.input} value={text} onChangeText={(text) => this.setState({ text })} />
+            <TouchableOpacity onPress={this.handleSave} style={styles.button}>
+              <Text style={styles.title_text}>Ekle</Text>
+            </TouchableOpacity>
+          </View>
+        
+        <View>
+          {data.map((item)=>{
+            return (<ItemList text={item.text}/>)
+            })
+        }
+        </View>
+      </View>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  title: {
+    backgroundColor: 'blue',
+    padding: 20,
+  },
+  title_text: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  input: {
+    padding: 10,
+    backgroundColor: 'white',
+    flex: 1,
+  },
+  button: {
+    padding: 10,
+    backgroundColor: 'blue',
+    borderRadius: 5,
+    marginLeft: 10,
+  },
+});
+
+//export default App;
